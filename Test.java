5010952a8c63eb43b20b3da6d3a99d8241d39b21
TMTMPST <vidoelag@gmail.com>
2023-08-30 20:55:31 +0700

    Just a test

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 2;
+        String b = "test";
+
+        String c = a + b;
+        System.out.print(c);
+    }
+}
